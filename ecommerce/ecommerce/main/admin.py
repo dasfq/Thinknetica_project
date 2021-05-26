6bfb3f3adb42780192476b657325d43eec421915
Dasfq <denis-966@mail.ru>
2021-05-27 02:28:05 +0500

    1.3. ckeditor install

--- ecommerce/ecommerce/main/admin.py
+++ ecommerce/ecommerce/main/admin.py
@@ -1,21 +1,31 @@
 from django.contrib import admin
+from django import forms
 from django.contrib.flatpages.models import FlatPage
 from django.contrib.flatpages.admin import FlatPageAdmin
 from django.utils.translation import gettext_lazy as _
+from ckeditor.fields import RichTextField
+from ckeditor.widgets import CKEditorWidget
+
+
+class FlatPageAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorWidget())
 
 
 class FlatPageAdmin(FlatPageAdmin):
-    fieldsets = (
-        (None, {'fields': ('url', 'title', 'content', 'sites')}),
-        (_('Advanced options'), {
-            'classes': ('collapse',),
-            'fields': (
-                'enable_comments',
-                'registration_required',
-                'template_name',
-            ),
-        }),
-    )
+    content = FlatPageAdminForm
+
+
+    # fieldsets = (
+    #     (None, {'fields': ('url', 'title', 'content', 'sites')}),
+    #     (_('Advanced options'), {
+    #         'classes': ('collapse',),
+    #         'fields': (
+    #             'enable_comments',
+    #             'registration_required',
+    #             'template_name',
+    #         ),
+    #     }),
+    # )
 
 admin.site.unregister(FlatPage)
 admin.site.register(FlatPage, FlatPageAdmin)
