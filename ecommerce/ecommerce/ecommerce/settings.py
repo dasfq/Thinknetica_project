809c78873fe7febf881718e17f504219d41aaf69
Dasfq <denis-966@mail.ru>
2021-05-27 01:38:12 +0500

    1.3. ckeditor install

--- ecommerce/ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/ecommerce/settings.py
@@ -55,6 +55,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    ## flatpages
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
 ]
 
 ROOT_URLCONF = 'ecommerce.urls'
--- ecommerce/ecommerce/main/admin.py
+++ ecommerce/ecommerce/main/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.flatpages.models import FlatPage
+from django.contrib.flatpages.admin import FlatPageAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+
+class FlatPageAdmin(FlatPageAdmin):
+    fieldsets = (
+        (None, {'fields': ('url', 'title', 'content', 'sites')}),
+        (_('Advanced options'), {
+            'classes': ('collapse',),
+            'fields': (
+                'enable_comments',
+                'registration_required',
+                'template_name',
+            ),
+        }),
+    )
+
+admin.site.unregister(FlatPage)
+admin.site.register(FlatPage, FlatPageAdmin)
