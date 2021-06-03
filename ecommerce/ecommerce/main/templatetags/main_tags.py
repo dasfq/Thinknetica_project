46f4812bbea88b3632e67ba8ae966bbd7ab44082
Dasfq <denis-966@mail.ru>
2021-06-04 00:30:29 +0500

    filter for reversing strings

--- ecommerce/ecommerce/main/templatetags/main_tags.py
+++ ecommerce/ecommerce/main/templatetags/main_tags.py
@@ -6,3 +6,8 @@ register = template.Library()
 @register.simple_tag
 def current_time(format_string):
     return datetime.datetime.now().strftime(format_string)
+
+@register.filter
+def convert_string(string):
+    string = str(string)
+    return "".join([x for x in string[::-1]])
