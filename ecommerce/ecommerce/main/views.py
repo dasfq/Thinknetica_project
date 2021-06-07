27704711c3ecb4e95ab7d6c6ee858a33d68d7896
Dasfq <denis-966@mail.ru>
2021-06-08 00:44:44 +0500

    tag links implementation

--- ecommerce/ecommerce/main/views.py
+++ ecommerce/ecommerce/main/views.py
@@ -17,11 +17,37 @@ def IndexView(request):
     }
     return render(request, template_name,context)
 
+class BaseListView():
+
+    def base_queryset(self, model_name):
+        tag = self.request.GET.get('tag')
+
+        if tag:
+            return model_name.objects.filter(tag__id=tag)
+        else:
+            return model_name.objects.all()
+
+    def base_get_tags(model_name):
+        tags_list = []
+        for ticket in model_name.objects.all():
+            for tag in ticket.tag.all():
+                tags_list.append(tag)
+        tags_list = set(tags_list)
+        return tags_list
+
 class CarList(ListView):
     model = TicketCar
     context_object_name = 'ticket_car_list'
     template_name = 'ticket_car_list.html'
     paginate_by = 10
+
+    def get_queryset(self):
+        return BaseListView.base_queryset(self, self.model)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tags_list'] = BaseListView.base_get_tags(self.model)
+        return context
 
 class CarDetailView(DetailView):
     model = TicketCar
@@ -35,6 +61,14 @@ class ServiceList(ListView):
     template_name = 'ticket_service_list.html'
     paginate_by = 10
 
+    def get_queryset(self):
+        return BaseListView.base_queryset(self, self.model)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tags_list'] = BaseListView.base_get_tags(self.model)
+        return context
+
 class ServiceDetailView(DetailView):
     model = TicketService
     template_name = 'ticket_service_detail.html'
@@ -46,6 +80,14 @@ class ItemList(ListView):
     context_object_name = "ticket_item_list"
     template_name = 'ticket_item_list.html'
     paginate_by = 10
+
+    def get_queryset(self):
+        return BaseListView.base_queryset(self, self.model)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tags_list'] = BaseListView.base_get_tags(self.model)
+        return context
 
 class ItemDetailView(DetailView):
     model = TicketItem
