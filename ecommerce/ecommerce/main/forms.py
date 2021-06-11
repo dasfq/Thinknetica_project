6c680a1d716c4d649f44a3bc595889ebf31b9550
Dasfq <denis-966@mail.ru>
2021-06-12 04:16:20 +0500

    Check ages 18 years old

--- ecommerce/ecommerce/main/forms.py
+++ ecommerce/ecommerce/main/forms.py
@@ -2,9 +2,16 @@ from django import forms
 from .models import Profile, TicketCar, TicketItem, TicketService, Picture
 from django.forms.models import inlineformset_factory
 from django.forms import modelformset_factory
-
+from datetime import date
 
 class ProfileForm(forms.ModelForm):
+
+    def clean_birth_date(self):
+        birth = self.cleaned_data['birth_date']
+        today = date.today()
+        if (birth.year + 18, birth.month, birth.day) > (today.year, today.month, today.day):
+            raise forms.ValidationError('Must be at least 18 years old to register')
+        return birth
 
     class Meta:
         model = Profile
