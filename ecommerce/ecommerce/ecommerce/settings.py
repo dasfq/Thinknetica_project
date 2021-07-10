2f1793d1bc0169b93195bbdb7c7a4f066062db94
Dasfq <denis-966@mail.ru>
2021-07-10 20:42:11 +0500

    caching in redis

--- ecommerce/ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/ecommerce/settings.py
@@ -212,3 +212,13 @@ beat_scheduler = 'DatabaseScheduler'
 TWILIO_ACCOUNT_SID = "AC61b71c449e9cff9c595b7bf0c7d21f1a"
 TWILIO_AUTH_TOKEN = "2a4f5e3c2dd59d7cb66b1f8f69a368d5"
 TWILIO_PHONE = '+16507537401'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
